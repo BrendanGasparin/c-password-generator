2a2a03a6a5bffe10f733a5b4d8826adc799bcbc7
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-05-31 01:04:35 +1000

    Program asks for user input int equal or greater than 8

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # c-password-generator
 A secure password generator made in C.
+
+## Installation
+```
+gcc -o passgen.exe passgen.c -lm
+```
--- passgen.c
+++ passgen.c
@@ -1,34 +1,43 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Input {
+typedef struct input {
     int n;
-    Input* next;
-} Input;
+    struct input *next;
+} input;
 
-int* get_input(char *prompt);
+struct input* get_input(char *prompt);
 
 int main(void) {
-    bool valid = true;
+    // TODO: Prevent integer overflow
+    bool valid;
+    int final_int;
     do {
-        struct Input *chars = get_int("Enter integer for password length. (Minimum 8): ");
-        struct Input *temp = chars;
+        struct input *chars = get_input("Enter integer for password length. (Minimum 8): ");
+        struct input *temp = chars;
         int char_count = 0;
+        final_int = 0;
+        valid = true;
 
-        if (chars->next == NULL)
+        if (chars == NULL) {
+            printf("Chars is NULL.\n");
             valid = false;
+        }
         else {
-            // Get character count
-            while(temp->next != NULL) {
+            while (temp != NULL) {
+                // Get character count
                 char_count++;
+                //if (temp->next != NULL)
                 temp = temp->next;
             }
-            char_count++;
 
             // Assign to an array
             int input[char_count];
-            for (int i = 0; i < char_count; i++) {
-                input[i] = chars->n;
+            temp = chars;
+            for (int i = char_count - 1; i >= 0; i--) {
+                input[i] = temp->n - 48;
                 if (temp->next != NULL)
                     temp = temp->next;
             }
@@ -41,31 +50,43 @@ int main(void) {
             }
 
             // Check all characters are ints
+            for (int i = 0; i < char_count; i++) {
+                if (input[i] < 0 || input[i] > 9) {
+                    valid = false;
+                }
+            }
+
+            // convert the array of single-digit ints into one multi-digit int
+            for (int i = 0; i < char_count; i++) {
+                final_int += input[char_count - i - 1] * (int)pow(10.0, (double)i);
+            }
+
+            // Input must be >= 8
+            if (final_int < 8)
+                valid = false;
         }
 
     } while (valid == false);
+
+    printf("You entered %d.\n", final_int);
 }
 
-int* get_input(char *prompt) {
-    struct Input* input = (struct Input*)malloc(sizeof(struct Input));
-    struct Input* temp = input;
+struct input* get_input(char *prompt) {
     int c;
     printf("%s", prompt);
 
+    struct input *input = NULL;
+
     // Create a linked list of user input in order of characters typed
     while ((c = getchar()) != EOF && c != '\n') {
-        if (input == NULL) {
-            input->n = c;
-            input->next = NULL;
+        struct input *temp = (struct input*)malloc(sizeof(struct input));
+        if (!temp) {
+            exit(EXIT_FAILURE);
         }
-        else {
-            while (temp->next != NULL) {
-                temp = temp->next;
-            }
-            input->n = c;
-            input->next = NULL;
-            temp->next = input;
-        }
+
+        temp->n = c;
+        temp->next = input;
+        input = temp;
     }
 
     return input;
